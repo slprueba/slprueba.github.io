af29d15669ca7277163eeb85e3aa50dddac37c9a
Matías Herrera Posse <matiasherreraposse0@gmail.com>
2023-02-02 15:39:20 -0300

    1 subida

---
+++ js/formulario.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+    // seleccionamos los elementos de la interfaz
+    const inputNombre = document.querySelector('#nombre');
+    const inputTelefono = document.querySelector('#telefono');
+    const inputEmail = document.querySelector('#email');
+    const inputAsunto = document.querySelector('#asunto');
+    const inputMensaje = document.querySelector('#mensaje');
+    const formulario = document.querySelector('.formulario');
+    console.log(formulario)
+
+})

e9287a8fabe245ac6851784d4246dac4a9785b2d
Matías Herrera Posse <matiasherreraposse0@gmail.com>
2023-02-08 12:26:51 -0300

    2 subida

--- js/scroll.js
+++ js/scroll.js
@@ -1,13 +1,25 @@
-const observer = new IntersectionObserver ((entries) =>{
-    entries.forEach((entry) => {
-        if(entry.isIntersecting){
-            entry.target.classList.add( 'show')
-        }else{
-            entry.target.classList.remove( 'show')
+    window.addEventListener('scroll', () =>{
+        const scrollPx = window.scrollY;
+        console.log(scrollPx);
+        const scroll = document.querySelector('.overlay');
+        
+        if(scrollPx > 1700){
+            scroll.style.backgroundColor = 'black';
+        }else if (scrollPx > 1000 ) {
+            scroll.style.backgroundColor = 'grey';
+        }else if (scrollPx > 0 ) {
+            scroll.style.backgroundColor = 'white';
         }
-    });
-});
+
+    })
 
 
-const hiddenElement = document.querySelectorAll('.hidden');
-hiddenElement.forEach((el)=> observer.observe(el));
+
+// const scroll = document.querySelector('.overlay');
+
+// scroll.addEventListener('scroll', cambioDeColor);
+
+// function cambioDeColor (){
+//     scroll.scroll
+//     console.log(scroll);
+// }
